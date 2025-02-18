db767d0dd5ebf250c261bb85d84544d742162a49
RODRIGO ROA URIBE <46855614+monoar2@users.noreply.github.com>
2025-02-17 19:36:07 -0600

    allowing prod host

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -18,5 +17,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    allowedHosts: ['elitemedicalbajio.online']
   },
 })
